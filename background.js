85b66197a936831bc9cfcdae2dce33e8c123c6bf
Aditibtp <aditisharmabtp@gmail.com>
2021-12-08 01:39:50 -0800

    initial container set up

---
+++ background.js
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This event is fired with the user accepts the input in the omnibox.
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
