d25931c225466808bc51d2045fa5506015342494
Aditibtp <aditisharmabtp@gmail.com>
2021-12-12 14:02:57 -0800

    added tasks to localStorage

--- LaundryList.js
+++ LaundryList.js
@@ -1,31 +1,76 @@
-var bodytag = document.getElementsByTagName("body")[0];
-
-bodytag.addEventListener("onload", attachfunctiontobody, false);
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
 
 function attachfunctiontobody(){
-    let someitem = document.getElementById("item-1");
-    let someitemimage = someitem.getElementsByTagName("img")[0];
+
+    var tasks = {
+        addedtasks: [],
+        donetasks: []
+    }
+
+    let someitem = document.getElementById("item-save-1");
+    let edititem = document.getElementById("item-edit-1");
 
     console.log("inside body")
 
-//for (var i = 0; i < lists.length; i++) {
-    //lists[i].addEventListener('click', handleListClick, false);
-    //lists[i].addEventListener('mouseover', handleMouseEnter, false);
-    //lists[i].addEventListener('mouseleave', handleMouseLeave, false);
-    //var closeImage = lists[i].querySelector("span.close-tab");
-    //closeImage.addEventListener("click", handleCloseClick, false);
-//}
+    someitem.addEventListener('click', handleSaveButtonClick, false);
 
-someitemimage.addEventListener('click', handleListClick, false);
+    edititem.addEventListener('click', handleEditButtonClick, false);
 
-    function handleListClick(){
+    function handleSaveButtonClick(){
         var textelement = document.getElementById("item-value-1");
         var textelementvalue = textelement.value;
+        
+        saveToLocalStorage(textelementvalue);
+
         var spanelement = document.createElement("span");
+        spanelement.className = "span-text";
         spanelement.textContent = textelementvalue;
         var divelement = document.getElementById("item-div-1");
         textelement.style.display = "none";
         
         divelement.appendChild(spanelement);
+
+        this.disabled = true;
+    }
+
+    function saveToLocalStorage(taskadded){
+        let tasksInStorage = localStorage.getItem("LaundryList");
+        let laundryListObj = JSON.parse(tasksInStorage);
+        console.log(laundryListObj);
+        laundryListObj.addedtasks.push(
+            {
+                taskString: taskadded,
+                timeAdded: Date.now()
+            }
+        );
+        tasks.addedtasks.push({taskString: taskadded, timeAdded: Date.now()});
+        localStorage.setItem("LaundryList", JSON.stringify(laundryListObj));
+    }
+
+    function handleEditButtonClick(){
+        var textelement = document.getElementById("item-value-1");
+        var textelementvalue = textelement.value;
+        var spanelement = document.createElement("span");
+        spanelement.className = "span-text";
+        spanelement.textContent = textelementvalue;
+        var divelement = document.getElementById("item-div-1");
+        textelement.style.display = "none";
+        
+        divelement.appendChild(spanelement);
+
+        this.disabled = true;
     }
 }
+
+function initiateExtn(){
+    document.addEventListener('DOMContentLoaded', function() {
+        attachfunctiontobody();
+    });
+};
+
+initiateExtn();
